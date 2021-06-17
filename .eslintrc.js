3a6cba365c307ef6d6a2483b2f00ef8d55233b2c
Fajarullah <frullah12@gmail.com>
2021-06-17 21:48:00 +0700

    inital commit
    inital commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  env: {	
+    browser: true,	
+    es2021: true
+  },	
+  extends: [	
+    'standard'	
+  ],	
+  parserOptions: {	
+    ecmaVersion: 12,	
+    sourceType: 'module'	
+  },	
+  rules: {}
+}
